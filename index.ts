962374cd2e0b55c4f429c83270bc884863f5a34a
ungvert <pavel.chelyapin@gmail.com>
2022-06-12 23:30:48 +0300

    feat: dev server with nodemon and ts-node

---
+++ index.ts
@@ -0,0 +1,12 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      data: "Hello World!",
+    })
+  );
+});
+
+server.listen(8000);
